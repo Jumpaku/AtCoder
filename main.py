b4d291b05f8341dd8d29b0fb53d2dbe1e4cb827c
Jumpaku <itzimeru@gmail.com>
2020-07-07 07:09:10 +0000

    feat(template.py): add odd and even

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import heapq as hq
 import sys
 import copy as cp
 sys.setrecursionlimit(10**9)
+input = sys.stdin.readline
 
 
 def dump_impl(*objects):
@@ -16,6 +17,12 @@ def dump_dummy(*objects):
 
 
 dump = dump_impl if "DEBUG" in sys.argv else dump_dummy
+
+
+def odd(n): return n & 1
+
+
+def even(n): return not odd(n)
 
 S = input()
 dump(S)
