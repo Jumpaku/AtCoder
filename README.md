13e7a8fc22ce71a0356ac809a389082c32033bf8
Jumpaku <Jumpaku@users.noreply.github.com>
2022-12-03 15:24:44 +0000

    feat(lib): dp

--- README.md
+++ README.md
@@ -4,25 +4,3 @@ Competitive programming environment that supports C++ 14 and Python 3.
 
 1. Open folder in container with VSCode Insiders.
 2. Type `make help` to see help.
-
-
-ac-gen --contest id
-ac-submit --problem p
-ac-submit_force --problem p
-
-test --work_dir p --entry main
-run --in i --out o --work_dir p --src main
-debug  --in i --out o --work_dir p --src main
-
-contest/
-    data/
-        current.id
-        arc128/
-    a/
-        bin/
-        test/
-            *.in
-            *.ans
-            *.out
-        main.cpp
-        main.py
---
+++ lib/dp.hpp
@@ -0,0 +1,108 @@
+/**
+ * DP
+ */
+#ifndef JUMPAKU_DP_HPP
+#define JUMPAKU_DP_HPP
+
+#ifndef TEMPLATE_HPP
+#include "../templates/template.hpp"
+#endif
+
+namespace dp {
+
+template <class Key, class Val> struct ICache {
+  virtual tuple<Val, bool> get(Key const &key) const = 0;
+  virtual void set(Key const &key, Val const &val) = 0;
+};
+template <class Key, class Val> struct CacheU : ICache<Key, Val> {
+  u_map<Key, Val> cache;
+  CacheU() : cache(2e6) {}
+  tuple<Val, bool> get(Key const &key) const override {
+    auto found = this->cache.find(key);
+    return found != cache.end() ? tuple<Val, bool>(found->second, true)
+                                : tuple<Val, bool>(Val{}, false);
+  }
+  void set(Key const &key, Val const &val) override { this->cache[key] = val; }
+};
+template <class Val> struct CacheV1 : ICache<tuple<ll>, Val> {
+  using Key = tuple<ll>;
+  CacheV1(ll n) : cache(n), cached(n) {}
+  vec<Val> cache;
+  vec<bool> cached;
+  tuple<Val, bool> get(Key const &key) {
+    auto const &[i] = key;
+    return make_tuple(cached[i], cache[i]);
+  }
+  void set(Key const &key, Val const &val) {
+    auto const &[i] = key;
+    this->cache[i] = val;
+    this->cached[i] = true;
+  }
+};
+template <class Val> struct CacheV2 : ICache<tuple<ll, ll>, Val> {
+  using Key = tuple<ll, ll>;
+  CacheV2(ll n0, ll n1) : cache(n0, vec<Val>(n1)), cached(n0, vec<bool>(n1)) {}
+  vec<vec<Val>> cache;
+  vec<vec<bool>> cached;
+  tuple<Val, bool> get(Key const &key) {
+    auto const &[i, j] = key;
+    return make_tuple(cached[i][j], cache[i][j]);
+  }
+  void set(Key const &key, Val const &val) {
+    auto const &[i, j] = key;
+    this->cache[i][j] = val;
+    this->cached[i][j] = true;
+  }
+};
+template <class Val> struct CacheV3 : ICache<tuple<ll, ll, ll>, Val> {
+  using Key = tuple<ll, ll, ll>;
+  CacheV3(ll n0, ll n1, ll n2)
+      : cache(n0, vec<vec<Val>>(n1, vec<Val>(n2))),
+        cached(n0, vec<vec<bool>>(n1, vec<bool>(n2))) {}
+  vec<vec<vec<Val>>> cache;
+  vec<vec<vec<bool>>> cached;
+  tuple<Val, bool> get(Key const &key) {
+    auto const &[i, j, k] = key;
+    return make_tuple(cached[i][j][k], cache[i][j][k]);
+  }
+  void set(Key const &key, Val const &val) {
+    auto const &[i, j, k] = key;
+    this->cache[i][j][k] = val;
+    this->cached[i][j][k] = true;
+  }
+};
+
+} // namespace dp
+using dp::CacheU;
+using dp::CacheV1;
+using dp::CacheV2;
+using dp::CacheV3;
+
+#endif
+/* end of DP */
+
+/**
+using Key;
+using Val;
+using Cache;
+using Data;
+struct DP {
+  DP(Data data, Cache cache) : data(data), cache(cache) {}
+  Data data;
+  Cache cache;
+  // define dp by following function
+  Val f(Key const &key) {
+    // base case
+    return Val();
+    // use cached ans
+    if (auto const &[ans, ok] = cache.get(key); ok) {
+      return ans;
+    }
+    // compute recursively
+    Val ans;
+    // cache ans
+    cache.set(key, ans);
+    return ans;
+  }
+};
+//*/
