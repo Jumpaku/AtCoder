d744609e399270e5e3d93511efa1ce6b6a1aa0d0
Jumpaku <Jumpaku@users.noreply.github.com>
2023-05-27 17:12:50 +0000

    feat: add repeat script

---
+++ tools/repeat.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+
+if [ $# != 2 ]; then
+    echo "positional arguments <count> and <job-command> are required"
+    echo "repeats execution of <job-command> <count> times, in which execution the job can take a 1-indexed environment variable LOOP_NUMBER"
+    exit 1
+fi
+
+set -eux
+
+COUNT=$1
+JOB_COMMAND=$2
+
+for LOOP_NUMBER in $(seq "${COUNT}"); do
+    export LOOP_NUMBER="${LOOP_NUMBER}"
+    sh -c "${JOB_COMMAND}"
+done
